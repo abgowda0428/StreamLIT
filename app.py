3fb57ba2a492aafc50c0419364179cdc0149764f
AB_GOWDA <abhishekgowda8090@gmail.com>
2025-10-27 22:22:31 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Hello Polar")
+st.subheader("Dommma Tagruuu")
+st.text("Your big Peaot")
+st.write("Chosse U r Polar")
+
+a = st.selectbox("Select Who are You :",["Big Peot","Tukali Tagruu","Big Pig","Dommma Tagruuu"])
+
+st.write(f"You Chosee {a}")
+
+st.success("You are peot")
